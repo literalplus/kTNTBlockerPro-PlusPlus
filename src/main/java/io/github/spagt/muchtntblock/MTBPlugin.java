5735ea4a451f8966dfa9ae08b54a78c689af943f
xxyy <xxyy@user.noreply.github.com>
2013-12-11 21:35:25 +0100

    Implement permissioning system. - If an explosion is caused by a TNTPrimed, its source Entity is checked for Permission, if applicable. - Explosions caused by other stuff (like World#spawnEntity() or Creepers) will be canceled anyway - increase swage level by 0.0056%

--- src/main/java/io/github/spagt/muchtntblock/MTBPlugin.java
+++ src/main/java/io/github/spagt/muchtntblock/MTBPlugin.java
@@ -1,14 +1,15 @@
 package io.github.spagt.muchtntblock;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.permissions.Permissible;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * sup, wurld?
- *
  */
 public class MTBPlugin extends JavaPlugin implements Listener {
 
@@ -19,6 +20,12 @@ public class MTBPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onTNTPrime(ExplosionPrimeEvent evt) {
+        if (evt.getEntity() instanceof TNTPrimed
+                && ( (TNTPrimed) evt.getEntity() ).getSource() instanceof Permissible
+                && !( (Permissible) ( (TNTPrimed) evt.getEntity() ).getSource() ).hasPermission("mtb.use-tnt")) {
+            return;
+        }
+
         evt.setCancelled(true);
         System.out.println("SPAGTMc was here! https://github.org/Spagt");
     }
